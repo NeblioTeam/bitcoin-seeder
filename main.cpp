e334eae3f3181604c2b431b349a8d6679d48ba18
NeblioTeam <neblioteam@gmail.com>
2018-01-31 21:28:42 +0000

    use only one node to seed

--- main.cpp
+++ main.cpp
@@ -348,6 +348,7 @@ static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
   db.Add(CService("178.62.247.189", fTestNet ? 16325 : 6325), true);
+//  db.Add(CService("128.199.205.137", fTestNet ? 16325 : 6325), true);
 
   do {
     for (int i=0; seeds[i] != ""; i++) {
